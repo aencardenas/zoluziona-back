821de1d5c3d52cc77f51ea217f55106d30a8c00c
Andres Cardenas <aencardenas@gmail.com>
2024-04-02 12:02:10 -0600

    feat: email backend request implementation

---
+++ src/main/java/com/sending/mail/controllers/EmailController.java
@@ -0,0 +1,25 @@
+package com.sending.mail.controllers;
+
+import com.sending.mail.services.IEmailService;
+import com.sending.mail.services.models.EmailDTO;
+import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class EmailController {
+    @Autowired
+    IEmailService emailService;
+
+    @PostMapping("/send-email")
+    private ResponseEntity<String> sendEmail(@RequestBody EmailDTO email) throws MessagingException {
+        emailService.sendMail(email);
+        return new ResponseEntity<>("Correo enviado", HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/sending/mail/services/impl/EmailServiceImpl.java
@@ -0,0 +1,51 @@
+package com.sending.mail.services.impl;
+
+import com.sending.mail.services.IEmailService;
+import com.sending.mail.services.models.EmailDTO;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+public class EmailServiceImpl implements IEmailService {
+    private final JavaMailSender javaMailSender;
+    private final TemplateEngine templateEngine;
+
+    public EmailServiceImpl(JavaMailSender javaMailSender, TemplateEngine templateEngine) {
+        this.javaMailSender = javaMailSender;
+        this.templateEngine = templateEngine;
+    }
+
+    @Override
+    public void sendMail(EmailDTO email) throws MessagingException {
+        try {
+
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setTo("math4everyone6@gmail.com");
+            helper.setSubject("Consulta de baterías Zoluziona");
+
+            Context context = new Context();
+            context.setVariable("message", email.getMessage());
+            context.setVariable("name", email.getName());
+            context.setVariable("lastName", email.getLastName());
+            context.setVariable("email", email.getEmail());
+            context.setVariable("phoneNumber", email.getPhoneNumber());
+
+            String contentHtml = templateEngine.process("email", context);
+
+            helper.setText(contentHtml, true);
+            javaMailSender.send(message);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro al enviar el correo: " + e.getMessage(),e);
+        }
+
+
+    }
+}
